ed03b787a557a71f2d5be81a2b467a0c358b94fd
pisesanket <85410203+pisesanket@users.noreply.github.com>
2023-10-02 17:05:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import '../styles/App.css';
-import { ThemeProvider } from './ThemeProvider';
-import { ThemeToggleButton } from './ThemeToggleButton';
-import { Page } from './Page'
+import React, { useState } from "react";
+import "../styles/App.css";
+import { ThemeProvider } from "./ThemeProvider";
+import { ThemeToggleButton } from "./ThemeToggleButton";
+import { Page } from "./Page";
 const App = () => {
-
+  const [theme, setTheme] = useState("light");
   return (
     <div id="main">
-      <ThemeProvider>
+      <ThemeProvider value={{ theme: theme, setTheme: setTheme }}>
         <Page />
         <ThemeToggleButton />
       </ThemeProvider>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
