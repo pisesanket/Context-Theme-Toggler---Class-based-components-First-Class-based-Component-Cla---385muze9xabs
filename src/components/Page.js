1cd1d69c98a6e3326416f43ebbbbf2302f5252f0
pisesanket <85410203+pisesanket@users.noreply.github.com>
2023-10-02 17:06:54 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import { LocalThemedBox } from './LocalThemedBox';
-import { ThemeContext } from './ThemeProvider';
+import React, { useContext } from "react";
+import { LocalThemedBox } from "./LocalThemedBox";
+import { ThemeContext } from "./ThemeProvider";
+import "../styles/App.css";
 
 const Page = () => {
+  const context = useContext(ThemeContext);
+  return (
+    <div
+      className={
+        "container " + `${context.theme === "light" ? "bg-light" : "bg-dark"}`
+      }
+      id="themed-page"
+    >
+      <p className={"txt-" + context.theme} id="themed-text-container">
+        lorem ipsum dolor iterit n stuff
+      </p>
+      <button className={"btn btn-" + context.theme} id="themed-button">
+        Themed Button
+      </button>
+      <LocalThemedBox />
+    </div>
+  );
+};
 
-    return(
-        <div className={"container"} id="themed-page">
-            <p id="themed-text-container">
-                lorem ipsum dolor iterit n stuff
-            </p>
-            <button className="btn" id="themed-button">Themed Button</button>
-            <LocalThemedBox />
-        </div>
-    )
-}
-
-export { Page }
+export { Page };
